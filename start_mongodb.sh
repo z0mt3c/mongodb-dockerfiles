a06bbef462b31a29e596e6b03b2fd186d791b616
Kamil Trzciński <ayufan@ayufan.eu>
2014-06-02 17:39:55 +0200

    Enhanced initialization process

--- start_mongodb.sh
+++ start_mongodb.sh
@@ -2,23 +2,21 @@
 
 set -e
 
+if [[ ! -f /opt/mongodb_password ]]; then
+    echo "No mongodb password defined"
+    exit 1
+fi
 if [[ ! -f /opt/mongodb/initialized ]]; then
     mkdir -p /opt/mongodb
     chown -R mongodb:mongodb /opt/mongodb
     chmod -R 755 /opt/mongodb
-fi
-if [[ ! -f /opt/mongodb_password ]]; then
-	echo "No mongodb password defined"
-	exit 1
-fi
-if [[ ! -f /opt/mongodb/initialized ]]; then
-	/usr/bin/mongod --dbpath=/opt/mongodb --noauth --fork --syslog
-	DB_PASSWORD="$(cat "/opt/mongodb_password")"
-	mongo <<EOF
+    /usr/bin/mongod --bind_ip=127.0.0.1 --dbpath=/opt/mongodb --noauth --fork --syslog
+    DB_PASSWORD="$(cat "/opt/mongodb_password")"
+    mongo <<EOF
 use admin
 db.addUser({user: "admin", pwd:"${DB_PASSWORD}", roles:["clusterAdmin", "userAdminAnyDatabase"]})
 EOF
-	kill $(pidof mongod)
+    kill $(pidof mongod)
     touch /opt/mongodb/initialized
 fi
 
