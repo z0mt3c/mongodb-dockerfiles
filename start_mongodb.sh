580017df643d3dfa63b6980b4f90ac906b7abc9c
Kamil Trzciński <ayufan@ayufan.eu>
2014-06-02 18:20:29 +0200

    Fixed MongoDB initialization

--- start_mongodb.sh
+++ start_mongodb.sh
@@ -8,15 +8,18 @@ if [[ ! -f /opt/mongodb_password ]]; then
 fi
 if [[ ! -f /opt/mongodb/initialized ]]; then
     mkdir -p /opt/mongodb
-    chown -R mongodb:mongodb /opt/mongodb
-    chmod -R 755 /opt/mongodb
     /usr/bin/mongod --bind_ip=127.0.0.1 --dbpath=/opt/mongodb --noauth --fork --syslog
     DB_PASSWORD="$(cat "/opt/mongodb_password")"
+    sleep 2s
+    echo "Creating admin user..."
     mongo <<EOF
 use admin
 db.addUser({user: "admin", pwd:"${DB_PASSWORD}", roles:["clusterAdmin", "userAdminAnyDatabase"]})
 EOF
     kill $(pidof mongod)
+    sleep 8s
+    chown -R mongodb:mongodb /opt/mongodb
+    chmod -R 755 /opt/mongodb
     touch /opt/mongodb/initialized
 fi
 
