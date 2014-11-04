4408d778a4b8a2bc144722da39c853199cacecb3
Kamil Trzcinski <ayufan@ayufan.eu>
2014-11-04 17:30:39 +0100

    Use smallfiles

--- start_mongodb.sh
+++ start_mongodb.sh
@@ -8,7 +8,7 @@ if [[ ! -f /opt/mongodb_password ]]; then
 fi
 if [[ ! -f /opt/mongodb/initialized ]]; then
     mkdir -p /opt/mongodb
-    /usr/bin/mongod --bind_ip=127.0.0.1 --dbpath=/opt/mongodb --noauth --fork --syslog
+    /usr/bin/mongod --bind_ip=127.0.0.1 --dbpath=/opt/mongodb --noauth --smallfiles --fork --syslog
     DB_PASSWORD="$(cat "/opt/mongodb_password")"
     sleep 2s
     echo "Creating admin user..."
@@ -25,4 +25,4 @@ fi
 chown -R mongodb:mongodb /opt/mongodb
 chmod 755 /opt/mongodb
 
-exec /usr/bin/mongod --dbpath=/opt/mongodb --auth
+exec /usr/bin/mongod --dbpath=/opt/mongodb --auth --smallfiles
